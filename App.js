36c9df50d720b52f7ab463d52585559f41c02176
annalaura99 <lalves.ana1031@gmail.com>
2024-04-02 22:04:28 -0300

    first commit

--- App.js
+++ App.js
@@ -1,36 +1,32 @@
 import 'react-native-gesture-handler';
-import React, { useEffect, useState, useRef } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens';
-import { decode, encode } from 'base-64';
-import database from '@react-native-firebase/database';
-import Chat from './Chat';
-import Chats from './Chats';
-
-if (!global.btoa) { 
-  global.btoa = encode;
-}
-if (!global.atob) { 
-  global.atob = decode;
-}
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  const messagesWrapperRef = useRef();
-  const [user, setUser] = useState({});
-  const [messages, setMessages] = useState([]);
-  const chatRef = database().ref('/applications/17/chat');
-  const messageRef = database().ref('/applications/3/chat/messages');
 
-  // Restante do seu código aqui
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Chats" component={Chats} />
-        <Stack.Screen name="Chat" component={Chat} />
+        { user ? (
+          <Stack.Screen name="Home">
+            {props => <HomeScreen {...props} extraData={user} />}
+          </Stack.Screen>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Registration" component={RegistrationScreen} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
